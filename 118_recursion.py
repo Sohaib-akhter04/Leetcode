43c838cf2470f2589bc53f6082cc1e28d482c6ba
Sohaib Akhter <akhtersohaib56@gmail.com>
2023-10-28 23:22:02 +0500

    Q118 with recursion

---
+++ 118_recursion.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution():
+    def generate(self,numRows:int)->List[List[int]]:
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+        prevRows=self.generate(numRows-1)
+        newRows=[1]*numRows
+        
+        for i in range(1,numRows-1):
+            newRows[i]=prevRows[-1][i]+prevRows[-1][i-1]
+        
+        prevRows.append(newRows)
+        return prevRows
+
+if __name__ == "__main__":
+    solution=Solution()
+    result=solution.generate(5)
+    print(result)
