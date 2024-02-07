a5422a7e35df7d011427831e21be5c397aea47eb
Sohaib akhter <k200292@nu.edu.pk>
2024-02-08 00:22:51 +0500

    startin 28

---
+++ 28.py
@@ -0,0 +1,14 @@
+#28. Find the Index of the First Occurrence in a String
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+
+if __name__=="__main__":
+    prices=[7,1,5,3,6,4]
+    solution=Solution()
