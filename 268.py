33ceb73a6fbe664249f091a3912d1269c3250e6b
Sohaib akhter <k200292@nu.edu.pk>
2024-02-11 04:20:25 +0500

    268 partial completed

--- 268.py
+++ 268.py
@@ -4,4 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        n=len(nums)
+        s=(n*(n+1))/2
+        Sum=sum(nums)
+        result=s-Sum
+        return int(result)
         
+if __name__ == "__main__":
+    solution=Solution()
+    nums=[3,0,2]
+    solution.missingNumber(nums)
