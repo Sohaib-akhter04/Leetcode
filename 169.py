2f120076a7cc5b801b36a8ea46a69e431665b409
Sohaib akhter <k200292@nu.edu.pk>
2024-02-10 17:49:28 +0500

    169.py majority element done

--- 169.py
+++ 169.py
@@ -1,3 +1,9 @@
+#Given an array nums of size n, return the majority element.
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+
+
+
 class Solution(object):
     def majorityElement(self, nums):
         """
@@ -19,6 +25,6 @@ class Solution(object):
 
 
 if __name__=="__main__":
-    solution=Solution()
+    solution=Solution() 
     nums=[3,3,4]
     solution.majorityElement(nums)
