05f1663ff72a0249cd200501a5a39d2ca9a086dd
Sohaib akhter <k200292@nu.edu.pk>
2024-02-11 18:13:30 +0500

    283 done

--- 283.py
+++ 283.py
@@ -7,4 +7,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        
+        non_zero_index=0
+        for index in range(len(nums)):
+            if nums[index]!=0:
+                nums[index], nums[non_zero_index] = nums[non_zero_index], nums[index]
+                non_zero_index+=1
+        print(nums)
+
+
+
+
+if __name__=="__main__":
+    solution=Solution()
+    nums=[1,3,0,0,2,0]
+    solution.moveZeroes(nums)
