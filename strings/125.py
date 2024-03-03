f9736d60033a833754f6e4901e69e57992624e6e
Sohaib akhter <k200292@nu.edu.pk>
2024-03-03 16:32:12 +0500

    ispalindrome done

--- strings/125.py
+++ strings/125.py
@@ -1,15 +1,11 @@
 class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        cleaned_s=''.join(char.lower() for char in s if char.isalnum())
-
+    def isPalindrome(self,s):
+        cleaned_s="".join(char.lower() for char in s if char.isalnum())
         return cleaned_s==cleaned_s[::-1]
+
 
 
 if __name__=="__main__":
     solution=Solution()
-    text= "101"
+    text="race a car"
     print(solution.isPalindrome(text))
