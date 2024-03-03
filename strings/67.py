1139b6cbc4ada8c35db503997793ea4b35a0c74b
Sohaib akhter <k200292@nu.edu.pk>
2024-03-03 15:56:58 +0500

    67 done

---
+++ strings/67.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        # a=int(a,2)
+        # b=int(b,2)
+        
+        return bin(int(a,2)+int(b,2))[2:]
+
+if __name__=="__main__":
+    solution=Solution()
+    a="11"
+    b="1"
+    print(solution.addBinary(a,b))
