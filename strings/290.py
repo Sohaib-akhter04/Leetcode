67e870a44b4f601b24062dd52dd8cdf60285083e
Sohaib akhter <k200292@nu.edu.pk>
2024-03-20 22:56:58 +0500

    290 done

--- strings/290.py
+++ strings/290.py
@@ -8,11 +8,31 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        words=s.split()
+        # print(words)
+        mapping={}
+        mapped_words=set()
+        if len(words)!=len(pattern):
+            return False
+        for word,char_s in zip(words,pattern):
+            if char_s in mapping:
+                if mapping[char_s]!=word:
+                    return False
+            else:
+                if word in mapped_words:
+                    return False
+                mapping[char_s]=word
+                mapped_words.add(word)
+        return True
 
 
+
+            
+
+       
 
 if __name__=="__main__":
     solution=Solution()
     pattern="abba"
-    s="dog cat cat dog"
+    s="dog cat ca dog"
     print(solution.wordPattern(pattern,s))
